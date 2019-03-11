2a947372fb6b2dcc7ae5cec985a67f78603292fd
Anatolii Grynchuk <agrynco@gmail.com>
2019-03-11 23:58:52 +0200

    #2 Added missed files

--- 4-digits-indicator/4-digits-indicator.ino
+++ 4-digits-indicator/4-digits-indicator.ino
@@ -1,24 +1,50 @@
-int pins[8] = {9, 13, 4, 6, 7, 10, 3, 5};
+#include "letters.h"
+#include "numbers.h"
 
-byte numbers[10] = 
+int pins[] = {9, 13, 4, 6, 7, 10, 3, 5};
+int pinDigits[] = {8, 11, 12, 2};
+
+char_2_mask_map char_2_mask_maps[] =
 {
-  B11111100,
-  B01100000,
-  B11011010,
-  B11110010,
-  B01100110,
-  B10110110,
-  B10111110,
-  B11100000,
-  B11111110,
-  B11110110
+	{'A', CHAR_A},
+	{'C', CHAR_C},
+	{'O', CHAR_O},
+	{'E', CHAR_E},
+	{'r', CHAR_r},
+	{'H', CHAR_H},
+	{'h', CHAR_h},
+	{'d', CHAR_d},
+	{'b', CHAR_b},
+	{'.', CHAR_DOT},
+	{' ', CHAR_SPACE},
+
+	{'0', ZERO},
+	{'1', ONE},
+	{'2', TWO},
+	{'3', THREE},
+	{'4', FOUR},
+	{'5', FIVE},
+	{'6', SIX},
+	{'7', SEVEN},
+	{'8', EIGHT},
+	{'9', NINE}
 };
 
-int number = 0;
+int const DELAY = 1;
 
-int pinDigits[4] = {8, 11, 12, 2};
+int loopsCounter = 0;
 
-int digit = 0;
+int numberToDisplay = 0;
+
+String textToDisplay = String(numberToDisplay);
+
+void showMask(byte mask)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		digitalWrite(pins[i], !bitRead(mask, 7 - i));
+	}
+}
 
 void setup() 
 {
@@ -31,36 +57,66 @@ void setup()
   for(int i = 0; i < 4; i++)
   {
     pinMode(pinDigits[i], OUTPUT);
-    digitalWrite(pinDigits[i], HIGH);
+    digitalWrite(pinDigits[i], LOW);
   }
+}
+
+void showMask(byte digitNumber, byte mask)
+{
+	showMask(mask);
+	digitalWrite(pinDigits[digitNumber], HIGH);
+	delay(DELAY);
+	digitalWrite(pinDigits[digitNumber], LOW);
+}
+
+byte getMask(const char c, boolean withDot)
+{
+	for (auto i = 0; i < sizeof(char_2_mask_maps) / sizeof(*char_2_mask_maps); i++)
+	{
+		if (c == char_2_mask_maps[i].c)
+		{
+			byte result = char_2_mask_maps[i].mask;
+			if (withDot)
+			{
+				result |= 1;
+			}
+
+			return result;
+		}
+	}
+
+	return CHAR_SPACE;
+}
+
+byte getMask(const char c)
+{
+	return getMask(c, false);
 }
 
 void loop() 
 {
-  number = (number + 1) % 10;
-  // number = 8;
-  
-  showNumber(number);
-  for (int i = 0; i < 4; i++)
-  {
-    digitalWrite(pinDigits[i], HIGH);
-    digit = random(0, 4);
-    digitalWrite(pinDigits[digit], LOW);
-    delay(1000);
-  }
+	loopsCounter++;
+	if ((loopsCounter % (100 / (textToDisplay.length() + 2))) == 0)
+	{
+		numberToDisplay++;
+		textToDisplay = String(numberToDisplay);
+		loopsCounter = 0;
+	}
+	
+	int digitNumber = 0;
+
+	for (byte i = 0; i < textToDisplay.length(); i++)
+	{
+		if (i < textToDisplay.length() -1)
+		{
+			if (textToDisplay.charAt(i) != '.' && textToDisplay.charAt(i + 1) == '.')
+			{
+				showMask(digitNumber, getMask(textToDisplay[i], true));
+				continue;
+			}
+		}
+		showMask(digitNumber, getMask(textToDisplay[i]));
+		digitNumber++;
+	}
 }
 
-void showNumber(int num)
-{
-  for(int i = 0; i < 8; i++)
-  {
-    if (bitRead(numbers[num], 7 - i) == HIGH)
-    {
-      digitalWrite(pins[i], LOW);
-    }
-    else
-    {
-      digitalWrite(pins[i], HIGH);
-    }
-  }
-}
---
+++ 4-digits-indicator/numbers.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+byte const ZERO = B11111100;
+byte const ONE = B01100000;
+byte const TWO = B11011010;
+byte const THREE = B11110010;
+byte const FOUR = B01100110;
+byte const FIVE = B10110110;
+byte const SIX = B10111110;
+byte const SEVEN = B11100000;
+byte const EIGHT = B11111110;
+byte const NINE = B11110110;
